89118e0e7c09b7a1e8e246eec55286ae315bad49
tonynguyen <tonyjsfreelancer@gmail.com>
2024-03-07 18:54:13 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+const opts = {
+  icons: {
+    iconfont: "mdi"
+  }
+};
+Vue.use(Vuetify);
+
+new Vue({
+  vuetify: new Vuetify(opts),
+  render: h => h(App)
+}).$mount("#app");
